26cc880001ece771d6b171787ded3ecdb15c61d8
Abdulkadir Akti <aakti@hsacarrollton.org>
2020-10-22 14:31:53 -0500

    editing button not finished yet

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,8 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { title } from 'process';
+import { Post } from '../post.model';
 
 import { PostsService } from "../posts.service";
 
@@ -10,19 +13,45 @@ import { PostsService } from "../posts.service";
 })
 //postu create etmek icin bir file actim orada iki variable tanimladim title ve content olarak
 
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit{
   enteredTitle = "";
   enteredContent = "";
-
+ mode ="create";
+ postId:string;
+  post:Post;
   //post la alakali seyleri post service clasinda hendle ettim sadece burda bi object olusturarak buraya ekledim
-  constructor(public postsService: PostsService) {}
+  constructor(public postsService: PostsService, public route:ActivatedRoute) {}
 
+  ngOnInit(){
+this.route.paramMap.subscribe((paramMap: ParamMap)=>{
+if (paramMap.has('postId')){
 
-  onAddPost(form: NgForm) {
+this.mode="edit";
+this.postId=paramMap.get('postId');
+this.postsService.getPost(this.postId).subscribe(postData=>{
+  this.post={id:postData._id ,title:postData.title, content:postData.content};
+});
+
+}else{
+  this.mode="create";
+  this.postId=null;
+}
+  });
+  }
+
+  onSavePost(form: NgForm) {
     if (form.invalid) {
       return;
+    }if(this.mode==="create")
+    { this.postsService.addPost(form.value.title, form.value.content);
+    }else{
+      this.postsService.updatePost(
+      this.postId,
+      form.value.title,
+      form.value.content
+       );
     }
-    this.postsService.addPost(form.value.title, form.value.content);
+
     form.resetForm();
   }
 }
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,64 +1,82 @@
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { pipe } from "rxjs";
-  import {  Subject } from "rxjs";
-import{map}from "rxjs/operators";
-import { Post } from "./post.model";
+  import { Injectable } from "@angular/core";
+  import { HttpClient } from "@angular/common/http";
+  import { pipe } from "rxjs";
+    import {  Subject } from "rxjs";
+  import{map}from "rxjs/operators";
+  import { Post } from "./post.model";
+  import { Title } from '@angular/platform-browser';
 
-@Injectable({ providedIn: "root" })// roota inject et
+  @Injectable({ providedIn: "root" })// roota inject et
 
-export class PostsService {
+  export class PostsService {
 
-  private posts: Post[] = []; //bi post create ettik
+    private posts: Post[] = []; //bi post create ettik
 
-  private postsUpdated = new Subject<Post[]>();//bide yeni post olusturursak onuda postun turunden yeniden olustur dedik
+    private postsUpdated = new Subject<Post[]>();//bide yeni post olusturursak onuda postun turunden yeniden olustur dedik
 
-  constructor(private http: HttpClient) {} //http requesti icin http yuo create ettin
+    constructor(private http: HttpClient) {} //http requesti icin http yuo create ettin
 
-  getPosts() {
-    this.http
-      .get<{ message: string; posts: any }>(
-        "http://localhost:3000/api/posts"
-      )
-      .pipe(map( (postData)=>{
-        return postData.posts.map(post=>{
-       return {
-          title:post.title,
-          content:post.content,
-          id:post._id
-           };
+    getPosts() {
+      this.http
+        .get<{ message: string; posts: any }>(
+          "http://localhost:3000/api/posts"
+        )
+        .pipe(map( (postData)=>{
+          return postData.posts.map(post=>{
+        return {
+            title:post.title,
+            content:post.content,
+            id:post._id
+            };
+          });
+        }))
+        .subscribe(transformendPosts => {
+          this.posts =transformendPosts;
+          this.postsUpdated.next([...this.posts]);
         });
-      }))
-      .subscribe(transformendPosts => {
-        this.posts =transformendPosts;
-        this.postsUpdated.next([...this.posts]);
-      });
-  }
-
-  getPostUpdateListener() {
-    return this.postsUpdated.asObservable();
-  }
-
-  addPost(title: string, content: string) {
-    const post: Post = { id: null, title: title, content: content };
-    this.http
-      .post<{ message: string,postId:string }>("http://localhost:3000/api/posts", post)
-      .subscribe(responseData => {
-     const id =responseData.postId;
-     post.id=id;
-        this.posts.push(post);
-        this.postsUpdated.next([...this.posts]);
-      });
     }
 
- deletePost(postId:string){
+    getPostUpdateListener() {
+      return this.postsUpdated.asObservable();
+    }
 
-   this.http.delete("http://localhost:3000/api/posts/"+ postId)
- .subscribe(()=>{
-   const updatedPosts= this.posts.filter(post=>post.id!==postId);
-   this.posts= updatedPosts;
-this.postsUpdated.next([...this.posts]);
- });
+    addPost(title: string, content: string) {
+      const post: Post = { id: null, title: title, content: content };
+      this.http
+        .post<{ message: string,postId:string }>("http://localhost:3000/api/posts", post)
+        .subscribe(responseData => {
+      const id =responseData.postId;
+      post.id=id;
+          this.posts.push(post);
+          this.postsUpdated.next([...this.posts]);
+        });
       }
+  getPost(id:string){
+    return this.http.get<{id:string, title:string,content:string}>("http://localhost:3000/api/posts/"+id);
+  }
 
-    }
+  updatePost(id:string,title:string,content:string){
+
+  const post:Post ={id:id,title:title, content:content};
+  this.http
+  .put("http://localhost:3000/api/posts/"+id,post)
+  .subscribe(response=>{
+    const updatedPosts=[...this.posts];
+    const oldPostIndex=updatedPosts.findIndex(p=>p.id===post.id);
+    updatedPosts[oldPostIndex]=post;
+    this.posts=updatedPosts;
+    this.postsUpdated.next([...this.posts]);
+  });
+  }
+
+
+  deletePost(postId:string){
+  this.http.delete("http://localhost:3000/api/posts/"+ postId)
+  .subscribe(()=>{
+    const updatedPosts= this.posts.filter(post=>post.id!==postId);
+    this.posts= updatedPosts;
+  this.postsUpdated.next([...this.posts]);
+  });
+        }
+
+      }

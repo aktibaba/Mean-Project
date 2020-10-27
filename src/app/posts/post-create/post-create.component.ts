3b7c13112a68f9e60ea8dfef240b4f4330a6276d
Abdulkadir Akti <aakti@hsacarrollton.org>
2020-10-27 14:46:46 -0500

    adding image with validator

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,90 +1,100 @@
-    import { Component, OnInit } from "@angular/core";
-    import { FormControl, FormGroup,Validators} from "@angular/forms";
-    import { ActivatedRoute, ParamMap } from '@angular/router';
-    import { title } from 'process';
-    import { Post } from '../post.model';
+      import { Component, OnInit } from "@angular/core";
+      import { FormControl, FormGroup,Validators} from "@angular/forms";
+      import { ActivatedRoute, ParamMap } from '@angular/router';
+      import { Post } from '../post.model';
+      import { PostsService } from "../posts.service";
+      import {mimeType} from "./mime-type.validator";
 
-    import { PostsService } from "../posts.service";
 
-    @Component({
-      selector: "app-post-create",
-      templateUrl: "./post-create.component.html",
-      styleUrls: ["./post-create.component.css"]
-    })
-    //postu create etmek icin bir file actim orada iki variable tanimladim title ve content olarak
+      @Component({
+        selector: "app-post-create",
+        templateUrl: "./post-create.component.html",
+        styleUrls: ["./post-create.component.css"]
+      })
+      //postu create etmek icin bir file actim orada iki variable tanimladim title ve content olarak
 
-    export class PostCreateComponent implements OnInit{
-      enteredTitle = "";
-      enteredContent = "";
-      mode ="create";
-      postId:string;
-      post:Post;
-      isLoading=false;
-      form:FormGroup;
-      imagePreview:string;
-      //post la alakali seyleri post service clasinda hendle ettim sadece burda bi object olusturarak buraya ekledim
-      constructor(public postsService: PostsService, public route:ActivatedRoute) {}
+      export class PostCreateComponent implements OnInit{
 
-      ngOnInit(){
+        //postun title ve contenti icin empty variable actik
+        enteredTitle = "";
+        enteredContent = "";
+        //postun bi edit bi create modu oldugu icin modunu create sabitledim ilk olarak
+        mode ="create";
+        postId:string;
+        post:Post;
+        //loading spinner icin bunu ekledik
+        isLoading=false;
+        //her postu bir form grup tan yaptik ki istedigimizi rahatca yapalim
+        form:FormGroup;
+        imagePreview:string;
+        //post la alakali seyleri post service clasinda hendle ettim sadece burda bi object olusturarak buraya ekledim
+        constructor(public postsService: PostsService, public route:ActivatedRoute) {}
 
-        this.form = new FormGroup({
-          'title': new FormControl(null,{
-            validators: [Validators.required,Validators.minLength(3)]}),
+        ngOnInit(){
 
-          'content': new FormControl(null,{
-            validators:[Validators.required]}),
+         //yukarida create ettigimiz form grubu burada actik ve validator ekledik
+          this.form = new FormGroup({
 
-          'image':new FormControl(null,{ validators:[Validators.required]}),
+            title: new FormControl(null,{ validators: [Validators.required,Validators.minLength(3)]}),
+            content: new FormControl(null,{validators:[Validators.required], asyncValidators:[mimeType]}),
+            image:new FormControl(null,{validators:[Validators.required]})
+
+            });
+
+
+//eger create ettigimiz postu edit etmek istiyorsak id si belilenmis postu al ve edit et
+
+          this.route.paramMap.subscribe((paramMap: ParamMap)=>{
+
+            if (paramMap.has('postId')){
+              this.mode="edit";
+              this.postId=paramMap.get('postId');
+              this.isLoading=true;
+              this.postsService.getPost(this.postId).subscribe(postData=>{
+                this.isLoading=false;
+                this.post={
+
+                  id:postData._id,
+                  title:postData.title,
+                   content:postData.content};
+
+                    this.form.setValue({'title':this.post.title,'content':this.post.content});
+                  });
+          }
+
+      else{
+        this.mode="create";
+        this.postId=null;
+      }
         });
-
-
-        this.route.paramMap.subscribe((paramMap: ParamMap)=>{
-
-          if (paramMap.has('postId')){
-             this.mode="edit";
-             this.postId=paramMap.get('postId');
-             this.isLoading=true;
-             this.postsService.getPost(this.postId).subscribe(postData=>{
-             this.isLoading=false;
-             this.post={id:postData._id,title:postData.title, content:postData.content};
-
-            this.form.setValue({
-        'title':this.post.title,
-        'content':this.post.content});
-    });
-
-    }else{
-      this.mode="create";
-      this.postId=null;
-    }
-      });
-      }
-      onImagePicked(event:Event){
-        const file=(event.target as HTMLInputElement).files[0];
-        this.form.patchValue({image:file});
-        this.form.get('image').updateValueAndValidity();
-        const reader= new FileReader();
-        reader.onload=()=>{
-          this.imagePreview= reader.result as string;
-        };
-        reader.readAsDataURL(file);
-      }
-      onSavePost() {
-
-        if (this.form.invalid) {
-          return;
         }
-        this.isLoading=true;
-        if(this.mode==="create")
-        { this.postsService.addPost(this.form.value.title, this.form.value.content);
-        }else{
-          this.postsService.updatePost(
-          this.postId,
-          this.form.value.title,
-          this.form.value.content
-          );
-        }
+        onImagePicked(event:Event){
 
-        this.form.reset();
+          const file=(event.target as HTMLInputElement).files[0];
+          this.form.patchValue({image:file});
+          this.form.get('image').updateValueAndValidity();
+          const reader= new FileReader();
+          reader.onload=()=>{
+            this.imagePreview= reader.result as string;
+          };
+          reader.readAsDataURL(file);
+        }
+        onSavePost() {
+
+          if (this.form.invalid) {
+            return;
+          }
+          this.isLoading=true;
+          if(this.mode==="create")
+          { this.postsService.addPost(this.form.value.title, this.form.value.content);
+          }else{
+            this.postsService.updatePost(
+            this.postId,
+            this.form.value.title,
+            this.form.value.content
+            );
+          }
+
+          this.form.reset();
+        }
       }
-    }
